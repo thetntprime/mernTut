ba42fe67c1593931457f95cc37d302dc5ef01a62
Jackie Zhuang <jackie_prime@hotmail.ca>
2024-10-21 17:53:18 -0400

    Created some semblance of a UI

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,9 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cors());
+
+/*
 app.use(
     cors({
         origin: '',
@@ -18,6 +21,7 @@ app.use(
         allowedHeaders: ['Content-Type']
     })
 );
+*/
 
 app.get('/', (request, response) => {
     console.log(request);
